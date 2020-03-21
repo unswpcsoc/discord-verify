0468421b57394bcbb62fe2d9cfb096522512a72a
Sabine Lim <thesabinelim@gmail.com>
2020-03-21 16:16:16 +1100

    Added default config

--- verifybot/config.py
+++ verifybot/config.py
@@ -1,10 +1,20 @@
 import yaml
 
-config_file = "../config.yml"
-with open(config_file, "r", encoding="utf-8") as fs:
-    config = yaml.load(fs)
+class ConfigFileNotFound(FileNotFoundError):
+    pass
+
+config_dir = "../config"
+config_file = f"{config_dir}/config.yml"
+
+try:
+    with open(config_file, "r", encoding="utf-8") as fs:
+        config = yaml.load(fs)
+except IOError as err:
+    raise ConfigFileNotFound("Can't find config file! Create a config.yml file \
+        in the config directory with similar structure to default.yml.")
 
 BOT_TOKEN = config["bot-token"]
 COMMAND_PREFIX = config["command-prefix"]
 ALLOWED_CHANNELS = config["allowed-channels"]
+ADMIN_CHANNELS = config["admin-channels"]
 ADMIN_ROLES = config["admin-roles"]
