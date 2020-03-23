6f700ebf900dec1da5a8736f180fc1e77e639316
Sabine Lim <thesabinelim@gmail.com>
2020-03-23 18:54:54 +1100

    Remove obsolete input request helpers

--- botcore/utils.py
+++ botcore/utils.py
@@ -1,6 +1,5 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from discord import DMChannel
 
 from botcore.config import config
 
@@ -16,25 +15,6 @@ async def request_yes_no(bot, user, prompt):
             and str(_reaction.emoji) in ["✅", "❌"]
     reaction = (await bot.wait_for("reaction_add", check=check))[0]
     return str(reaction.emoji) == "✅"
-
-# DM user with prompt.
-# Return the text contents of their next message.
-async def request_input(bot, user, prompt):
-    await user.send(prompt)
-    def check(_message):
-        return _message.author.id == user.id \
-            and isinstance(_message.channel, DMChannel)
-    return (await bot.wait_for("message", check=check)).content
-
-# DM user with prompt.
-# Return the next list of attachments received.
-async def request_attachments(bot, user, prompt):
-    await user.send(prompt)
-    def check(_message):
-        return _message.author.id == user.id \
-            and isinstance(_message.channel, DMChannel) \
-            and len(_message.attachments) > 0
-    return (await bot.wait_for("message", check=check)).attachments
 
 # Send message as an email with title subject to recipient (an email address).
 def send_email(mail_server, recipient, subject, message):
